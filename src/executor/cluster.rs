31d0fa0a2dff655eaab5a8b9de5cda62650a7fc8
AXEwiges <2495477644@qq.com>
2023-03-03 00:38:54 +0800

    Add some modules to package.The project plan has been completed.

---
+++ src/executor/cluster.rs
@@ -0,0 +1,3 @@
+pub fn execution(task_id: i64) {
+    print!("{task_id}");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use night::executor::local::execution as exec;
+use std::process::Command;
+
+fn sys_info() {
+    let result = Command::new("ls").output().expect("normal");
+    let str_1 = String::from_utf8(result.stdout);
+    println!("{str_1:?}");
+}
+
 fn main() {
     println!("Hello, world!");
+    exec(14);
+    sys_info();
 }
